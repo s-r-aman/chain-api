d2e11359e95d2f4f38724926e300ba34dff03dc2
s-r-aman <s.r.aman.sra@gmail.com>
2018-06-15 22:20:23 +0530

    Added graphql related code.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use('/graphql', bodyParser.json())
+app.use('/graphql', bodyParser.json(), graphqlExpress({}))
+app.use('/graphiql', graphiqlExpress({ endpointURL: 'graphql' }))
 
 app.get('/', (req, res) => {
   res.send('You can make your GraphQL request at /graphql')
