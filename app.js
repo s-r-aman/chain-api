632ddc35b73f6d3f7466f3c5bb83b421fce1c55a
s-r-aman <s.r.aman.sra@gmail.com>
2018-06-16 22:38:18 +0530

    Changed code for adding schema.

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
+const { makeExecutableSchema } = require('graphql-tools')
+
+const { typeDefs } = require('./users/Schema')
+const { resolvers } = require('./users/resolver')
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({}))
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }))
 app.use('/graphiql', graphiqlExpress({ endpointURL: 'graphql' }))
 
 app.get('/', (req, res) => {
-  res.send('You can make your GraphQL request at /graphql')
+  res.send('You can make your GraphQL request at /graphiql')
 })
 
+//  eslint-disable-next-line
 app.listen(PORT, () => console.log(`Server is up -> ${PORT}`))
