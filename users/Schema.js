9c3c9aeee14ca2fda85f6bc05b62f1783952db9a
s-r-aman <s.r.aman.sra@gmail.com>
2018-06-17 16:18:53 +0530

    Changed users schema.

--- users/Schema.js
+++ users/Schema.js
@@ -1,15 +1,14 @@
-const typeDefs = `
-  type Query { users(_id: ID): User}
+const UserSchema = `
   type User {
     _id: ID,
     userName: String,
     name: String,
     age: Int,
     gender: String,
-    habits: [ID]
+    habits: [Habit]
   }
 `
 
 //  TODO: Make gender ENUM
 
-module.exports = { typeDefs }
+module.exports = { UserSchema }
