93c7d422071f926328e71c03d7aed4199284f6f5
s-r-aman <s.r.aman.sra@gmail.com>
2018-06-20 12:58:26 +0530

    Added database ingreation for signup.

--- users/resolver.js
+++ users/resolver.js
@@ -1,3 +1,7 @@
+const db = require('./../database/database')
+
+const users = db('users')
+
 const User = [
   {
     _id: '7379896908',
@@ -11,10 +15,27 @@ const User = [
 
 const login = (_, { password }) => User.find(user => user.password === password)
 
-const signUp = (_, { userName, password, age, gender, name }) => {
-  User.push({ userName, password, age, gender, name })
-  return User.find(user => user.password === password)
-}
+const signUp = (_, { username, password, age, gender, name }) =>
+  users
+    .insert({
+      username,
+      password,
+      age,
+      gender,
+      name,
+      created_at: new Date(),
+      updated_at: new Date()
+    })
+    .returning([
+      'username',
+      'password',
+      'age',
+      'gender',
+      'name',
+      'created_at',
+      'updated_at'
+    ])
+    .then(([firstElement]) => firstElement)
 
 const getUser = (_, { token }) =>
   User.find(({ token: authToken }) => token === authToken)
