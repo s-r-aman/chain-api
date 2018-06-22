e04cca9c75965f3776811c08324dd32e110f79a8
s-r-aman <s.r.aman.sra@gmail.com>
2018-06-22 12:34:03 +0530

    Refactoring in code for depedency injection.

--- utils/utils1.js
+++ utils/utils1.js
@@ -1,16 +1,20 @@
 const bcrypt = require('bcryptjs')
 
-const generateHash = password =>
-  bcrypt
+const generateHashMin = dependency => password =>
+  dependency
     .genSalt(10)
-    .then(salt => bcrypt.hash(password, salt))
+    .then(salt => dependency.hash(password, salt))
     .then(hash => Promise.resolve(hash))
     .catch(err => Promise.reject(err))
 
-const compareHash = (user, password) =>
-  bcrypt
+const compareHashMin = dependency => (user, password) =>
+  dependency
     .compare(password, user.password)
     .then(result => Promise.resolve({ result, user }))
     .catch(err => Promise.reject(err))
 
-module.exports = { generateHash, compareHash }
+const generateHash = generateHashMin(bcrypt)
+
+const compareHash = compareHashMin(bcrypt)
+
+module.exports = { generateHash, compareHash, generateHashMin, compareHashMin }
