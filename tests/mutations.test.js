abbea1ba1ca92997d76eb5ca5e8707a5b6deba43
s-r-aman <s.r.aman.sra@gmail.com>
2018-06-28 16:27:39 +0530

    Written more tests.

--- tests/mutations.test.js
+++ tests/mutations.test.js
@@ -3,12 +3,17 @@ const app = require('./../app')
 const db = require('./../database/database')
 
 const {
-  users: [firstUser]
+  users: [firstUser, secondUser]
 } = require('./__fixtures__/Users')
 
-const graphql = request(app).post('/graphql')
+const graphql = query =>
+  request(app)
+    .post('/graphql')
+    .send({ query })
+
 const usersDB = db('users')
 const tokenDB = db('tokens')
+const query = condition => db('tokens').where(condition)
 
 const queries = [
   `mutation {
@@ -25,33 +30,44 @@ const queries = [
           gender,
           token
         }
+      }`,
+  `mutation {
+        signUp(
+          username: "${secondUser.username}",
+          name: "${secondUser.name}",
+          password: "${secondUser.password}",
+        ){
+          username,
+          name,
+          age,
+          gender,
+          token
+        }
       }`
 ]
 
 afterEach(() => usersDB.delete())
+afterEach(() => tokenDB.delete())
 
 describe('Mutations', () => {
   describe('signUp', () => {
     test('sign the new user up.', () =>
-      graphql
-        .send({
-          query: queries[0]
-        })
-        .then(({ body: { data: { signUp } } }) =>
-          expect({ ...signUp, password: firstUser.password }).toMatchObject(
-            firstUser
-          )
-        ))
+      graphql(queries[0]).then(({ body: { data: { signUp } } }) =>
+        expect({ ...signUp, password: firstUser.password }).toMatchObject(
+          firstUser
+        )
+      ))
     test('return the correct token.', () =>
-      graphql
-        .send({
-          query: queries[0]
+      graphql(queries[0]).then(({ body: { data: { signUp } } }) => {
+        expect(signUp).toHaveProperty('token')
+        expect(signUp).not.toHaveProperty('password')
+        return query({ username: signUp.username }).then(([{ token }]) => {
+          expect(token).toBe(signUp.token)
         })
-        .then(({ body: { data: { signUp } } }) => {
-          expect(signUp).toHaveProperty('token')
-          return tokenDB
-            .where('username', signUp.username)
-            .then(([{ token }]) => expect(token).toBe(signUp.token))
-        }))
+      }))
+    test('give error on giving less arguments.', () =>
+      graphql(queries[1]).then(({ body }) =>
+        expect(body).toHaveProperty('errors')
+      ))
   })
 })
