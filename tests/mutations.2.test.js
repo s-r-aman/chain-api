7df8b331cd11a7a8e51e87b8d66730e313e78e8d
s-r-aman <s.r.aman.sra@gmail.com>
2018-07-10 18:54:42 +0530

    Added more tests.

--- tests/mutations.2.test.js
+++ tests/mutations.2.test.js
@@ -4,7 +4,13 @@ const app = require('./../app')
 const [firstUser] = require('./__fixtures__/Users')
 const [firstHabit] = require('./__fixtures__/Habits')
 
-const [signUpQuery, , , addHabitQuery] = require('./helpers/queries')
+const [
+  signUpQuery,
+  ,
+  ,
+  addHabitQuery
+  // updateHabitQuery
+] = require('./helpers/queries')
 
 const graphql = query =>
   request(app)
@@ -18,12 +24,29 @@ describe('Mutations.', () => {
         .then(({ body: { data: { signUp } } }) =>
           graphql(addHabitQuery({ token: signUp.token, ...firstHabit }))
         )
-        .then(({ body: { data } }) => {
+        .then(({ body: { data, error } }) => {
+          if (error) {
+            console.log(error)
+          }
           // FIXME: reminder: new Date(data.addHabit.reminder).toISOString()
-          expect({
-            ...data.addHabit,
-            reminder: new Date(firstHabit.reminder).toISOString()
-          }).toMatchObject(firstHabit)
+          expect(data.addHabit).toMatchObject({
+            ...firstHabit,
+            reminder: data.addHabit.reminder
+          })
+          expect(data.addHabit.id).toBeDefined()
+          expect(typeof data.addHabit.id).toBe('string')
         }))
+    test('Give error when wrong token is passed.', () =>
+      graphql(addHabitQuery({ token: 'token', ...firstHabit })).then(
+        ({ body: { data, errors } }) => {
+          expect(data.addHabit).toBeNull()
+          expect(errors).toBeDefined()
+        }
+      ))
   })
+  // describe('updateHabit', () => {
+  //   test('Update the habit.', () => {
+  //     graphql(updateHabitQuery)
+  //   })
+  // })
 })
