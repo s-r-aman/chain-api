5e1d8e8defe1487263ff7abe7c19f78010c9f884
s-r-aman <s.r.aman.sra@gmail.com>
2018-07-10 18:29:13 +0530

    Added resolver for read, update and delete in habits.

--- habits/resolver.js
+++ habits/resolver.js
@@ -46,6 +46,69 @@ const addHabit = async (
   return { ...habit[0] }
 }
 
-const mutations = { addHabit: catchErrors(addHabit) }
+const updateHabit = async (
+  _,
+  { token, id, name, diff_level, icon, reminder, updated_at = new Date() }
+) => {
+  const [{ username }] = await db('tokens').where({ token })
+  const habit = await db('habits')
+    .where({ id, username })
+    .update(
+      {
+        username,
+        name,
+        icon,
+        diff_level,
+        reminder,
+        updated_at
+      },
+      [
+        'id',
+        'name',
+        'diff_level',
+        'icon',
+        'reminder',
+        'coins',
+        'diamonds',
+        'completed_dates',
+        'created_at',
+        'updated_at'
+      ]
+    )
+  return { ...habit[0] }
+}
 
-module.exports = { mutations }
+const deleteHabit = async (_, { token, id }) => {
+  const [{ username }] = await db('tokens').where({ token })
+  const habit = await db('habits')
+    .where({ id, username })
+    .del([
+      'id',
+      'name',
+      'diff_level',
+      'icon',
+      'reminder',
+      'coins',
+      'diamonds',
+      'completed_dates',
+      'created_at',
+      'updated_at'
+    ])
+  return { ...habit[0] }
+}
+
+const getHabit = async (_, { token, id }) => {
+  const [{ username }] = await db('tokens').where({ token })
+  const habit = await db('habits').where({ id, username })
+  return { ...habit[0] }
+}
+
+const queries = { getHabit: catchErrors(getHabit) }
+
+const mutations = {
+  addHabit: catchErrors(addHabit),
+  updateHabit: catchErrors(updateHabit),
+  deleteHabit: catchErrors(deleteHabit)
+}
+
+module.exports = { mutations, queries }
